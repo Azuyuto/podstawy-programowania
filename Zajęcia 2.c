c87ba542f0efea3a7dd98c0ca8577733faafe1b6
Azuyuto <49310271+Azuyuto@users.noreply.github.com>
2020-12-04 21:28:14 +0100

    Add files via upload

---
+++ Zajęcia 2.c
@@ -0,0 +1,136 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    // Task 1, 0.5pkt
+    //int tab[3];
+    //tab[0]=1;
+    //printf("tab[0] = %d\n", tab[0]);
+    //printf("tab[1] = %d\n", tab[1]);
+    //printf("tab[2] = %d\n", tab[2]);
+
+    // Task 2, 2pkt
+    //printf("Jestes hardkorem byczku? (wpisz cyfre) \n");
+    //printf("+");
+    //int i;
+    //scanf("%d", &i);
+    //if(i == 1)
+    //{
+    //    printf("I o to chodzi ;)");
+    //}
+    //else
+    //{
+    //    printf("Przykro mi, ojoj ;(");
+    //}
+
+    // Task 3
+    //int option;
+    //const float milaLadowa = 1.609, zmienna;
+    //printf("Wybierz opcje: \n");
+    //printf("1. Mile na km. \n");
+    //printf("2. Km na mile. \n");
+    //scanf("%d", &option);
+    //if(option == 1)
+    //{
+    //    printf("Podaj ile mil: ");
+    //    scanf("%f", &zmienna);
+    //    printf("Km: %f", zmienna*milaLadowa);
+    //}
+    //else if(option == 2)
+    //{
+    //    printf("Podaj ile km: ");
+    //    scanf("%f", &zmienna);
+    //    printf("Mile: %f", zmienna/milaLadowa);
+    //}
+    //else
+    //{
+    //    printf("UPS ##, wybrales zla opcje. \n");
+    //}
+
+    // Task 4
+    //int i;
+    //scanf("%d", &i);
+    //if(i = 2);
+    //{
+    //    printf("Wpisales 2");
+    //}
+
+    // Task 5
+    //char odp;
+    //printf("Co wywoluje kile? \n");
+    //printf("A. paleczka. \n");
+    //printf("B. laseczka \n");
+    //printf("C. pratek \n");
+    //printf("D. kretek \n");
+    //scanf("%s", &odp);
+    //if(odp == 'C' || odp == 'c')
+    //{
+    //    printf("\nDobra opdowiedz, jestes na progu 10 000, przechodzimy do nastepnego pytania...\n");
+    //   printf("\nNa fladze ktorego kraju widnieje przyprawa do pasztetu? \n");
+    //    printf("A. Hiszpanii. \n");
+    //    printf("B. Grenady \n");
+    //    printf("C. Chin \n");
+    //    printf("D. Szwecji \n");
+    //    scanf("%s", &odp);
+    //    if(odp == 'B' || odp == 'b')
+    //    {
+    //        printf("\nDobra opdowiedz, wygrywasz 1 000 000.\n");
+    //        return 0;
+    //    }
+    //}
+    //printf("Zla odpowiedz, przegrywasz, tracisz wszystko.\n");
+
+    // Task 6, 2pkt
+    //int tab1[3];
+    //tab1[0] = 1;
+    //tab1[1] = 2;
+    //tab1[2] = 3;
+    //int tab2[3];
+    //tab2[0] = 4;
+    //tab2[1] = 5;
+    //tab2[2] = 6;
+    //
+    //// mearch to:
+    //int result[6];
+    //for(int i=0;i<2;i++)
+    //{
+    //    for(int j=0;j<3;j++)
+    //    {
+    //        if(i==0)
+    //        {
+    //            result[j] = tab1[j];
+    //        }
+    //        else
+    //        {
+    //           result[j+3] = tab2[j];
+    //        }
+    //    }
+    //}
+    //// input result
+    //for(int i=0;i<6;i++)
+    //{
+    //    printf("result[%d]: %d\n", i, result[i]);
+    //}
+
+    // Task 7
+    //float begin;
+    //float end;
+    //int month;
+
+    //printf("Kwota poczatkowa: ");
+    //scanf("%f", &begin);
+    //printf("Kwota koncowa: ");
+    //scanf("%f", &end);
+    //printf("Miesiace: ");
+    //scanf("%d", &month);
+
+    //printf("(%%) zysku/straty: %.2f%%\n", ((end-begin)/begin)*100);
+    //printf("(%%) zysku/straty na miesiac: %.2f%%", (((end-begin)/begin)*100)/month);
+
+    //return 0;
+}
+
+
+
+
